9bfa3826c85a2f4fbdc52bdbdd8fefbd81bd5464
rmartine <rmartine@redhat.com>
2017-04-17 11:43:17 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+import os
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+
+	
+	textfile = spark.read.text( os.environ['SPARK_HOME'] + "/README.md")
+
+    lines = textfile.rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+
+spark.stop()
